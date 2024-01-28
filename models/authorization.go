678fd33f9028815bea610f7e05699b10a18958ba
Rajeev Chourey <rajeevchourey786@gmail.com>
2024-01-28 19:07:35 +0530

    added authorization input output models

--- models/authorization.go
+++ models/authorization.go
@@ -8,8 +8,18 @@ import (
 type AuthorizationLog struct {
 	UserId      string             `json:"userId"`
 	Role        constants.Role     `json:"role"`
-	Actions     []constants.Action `json:"action"`
+	Actions     []constants.Action `json:"actions"`
 	RequestedAt time.Time          `json:"requestedAt"`
 	Grant       constants.Grant    `json:"grant"`
 	Reason      string             `json:"reason"`
+}
+
+type AuthorizationInput struct {
+	Token   string             `json:"token"`
+	Actions []constants.Action `json:"actions"`
+}
+
+type AuthorizationOutput struct {
+	Grant  constants.Grant `json:"grant"`
+	UserId string          `json:"userId"`
 }
